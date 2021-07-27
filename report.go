e1cfdebffcb577ef6855417fd1f7eb17c507c30c
Joel Jensen <yobert@gmail.com>
2021-07-26 21:22:03 -0600

    Much better report view, but still lacking some things

--- report.go
+++ report.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"os"
 	"sort"
+	"strings"
+
+	"github.com/go-ldap/ldap/v3"
 )
 
 var tmpl = template.Must(template.New("").Parse(`
@@ -12,202 +16,256 @@ var tmpl = template.Must(template.New("").Parse(`
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>{{ .Title }}</title>
+		<title>LDAP report</title>
 		<style>
 html, body {
 	margin: 0;
 	padding: 0;
+	font-family: sans-serif;
 }
-.flex {
-	display: flex;
-	flex-wrap: wrap;
+.children {
+	margin-left: 4rem;
 }
-.group {
-	flex-grow: 1;
-	border: 1px solid black;
-	border-radius: 1rem;
-	margin: 1rem 1rem 0 0;
+.children_closed {
+	display: none;
 }
-.groupname {
-	font-weight: bold;
-	padding: 0.25rem 0.5rem;
+.name_toggle {
+	display: block;
+	color: black;
+	text-decoration: none;
 }
-.groupusers {
-	border-top: 1px solid black;
-	max-height: 20rem;
-	overflow-y: auto;
-	padding: 0.25rem 0.5rem;
+.plus {
+	font-family: monospace;
+}
+a > .name {
+	color: #05a;
+}
+.count {
+	opacity: 0.5;
+}
+.count_zero {
+	display: none;
 }
 		</style>
 		<script>
+function toggle(id) {
+	var plus = document.getElementById("plus_"+id)
+	if(!plus)
+		return
+
+	var div = document.getElementById("children_"+id)
+	if(!div)
+		return
+
+	if(div.className == "children children_closed") {
+		div.className = "children"
+		plus.innerHTML = "-"
+	} else {
+		div.className = "children children_closed"
+		plus.innerHTML = "+"
+	}
+}
 		</script>
 	</head>
 	<body>
 
-<div class="flex">
-{{ range .Groups }}
-	<div class="group">
-		<div class="groupname" title="{{ .DN }}">{{ .Name }}</div>
-{{ if .Users }}
-		<div class="groupusers">
-{{ range .Users }}
-			<div class="groupuser" title="{{ .DN }}">{{ .Name }}</div>
+{{ define "node" }}
+{{ if .DN }}
+<div class="node">
+
+{{ if .Children }}
+{{ if gt (len .Children) 5 }}
+	<a class="name_toggle" title="{{ .DN }}" href="#" onclick="toggle({{ .ID }}); return false"><span class="plus" id="plus_{{ .ID }}">+</span> <span class="name">{{ .Name }}</span> <span class="count">{{ len .Children }}</span></a>
+{{ else }}
+	<a class="name_toggle" title="{{ .DN }}" href="#" onclick="toggle({{ .ID }}); return false"><span class="plus" id="plus_{{ .ID }}">-</span> <span class="name">{{ .Name }}</span> <span class="count">{{ len .Children }}</span></a>
 {{ end }}
-		</div>
+{{ else }}
+	<span class="name_toggle" title="{{ .DN }}" href="#" onclick="toggle({{ .ID }}); return false"><span class="plus" id="plus_{{ .ID }}">&nbsp;</span> <span class="name">{{ .Name }}</span> <span class="count count_zero">{{ len .Children }}</span></span>
 {{ end }}
-	</div>
+
+{{ if .Children }}
+
+{{ if gt (len .Children) 5 }}
+<div class="children children_closed" id="children_{{ .ID }}">
+{{ else }}
+<div class="children" id="children_{{ .ID }}">
 {{ end }}
+
+{{ range .Children }}
+{{ template "node" . }}
+{{ end }}
+
 </div>
-
-	</body>
-</html>
-`))
-
-var tmpl2 = template.Must(template.New("").Parse(`
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>{{ .Title }}</title>
-		<style>
-html, body {
-	margin: 0;
-	padding: 0;
-}
-.flex {
-	display: flex;
-	flex-wrap: wrap;
-}
-.group {
-	flex-grow: 1;
-	border: 1px solid black;
-	border-radius: 1rem;
-	margin: 1rem 1rem 0 0;
-}
-.groupname {
-	font-weight: bold;
-	padding: 0.25rem 0.5rem;
-}
-.groupusers {
-	border-top: 1px solid black;
-	max-height: 20rem;
-	overflow-y: auto;
-	padding: 0.25rem 0.5rem;
-}
-		</style>
-		<script>
-		</script>
-	</head>
-	<body>
-
-<div class="flex">
-{{ range .Users }}
-	<div class="group">
-		<div class="groupname" title="{{ .DN }}">{{ .Name }}</div>
-{{ if .Groups }}
-		<div class="groupusers">
-{{ range .Groups }}
-			<div class="groupuser" title="{{ .DN }}">{{ .Name }}</div>
 {{ end }}
-		</div>
-{{ end }}
-	</div>
-{{ end }}
+
 </div>
+{{ else }}
+{{ range .Children }}
+{{ template "node" . }}
+{{ end }}
+{{ end }}
+{{ end }}
+
+{{ template "node" . }}
+
 	</body>
 </html>
 `))
 
 type tmplData struct {
-	Title  string
-	Users  []*tmplDataUser
-	Groups []*tmplDataGroup
+	ID          string
+	DN          string
+	SubDN       string
+	Name        string
+	Children    []*tmplData
+	ChildrenMap map[string]*tmplData
 }
-type tmplDataUser struct {
-	DN     string
-	Name   string
-	Groups []*tmplDataGroup
-}
-type tmplDataGroup struct {
-	DN    string
-	Name  string
-	Users []*tmplDataUser
+
+func sortdata(data *tmplData) {
+	keys := make([]string, 0, len(data.ChildrenMap))
+	children := make([]*tmplData, 0, len(data.ChildrenMap))
+	for key := range data.ChildrenMap {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	for _, key := range keys {
+		child := data.ChildrenMap[key]
+		children = append(children, child)
+		sortdata(child)
+	}
+	data.Children = children
 }
 
 func report(data *Data) error {
-	users := data.Users
-	groups := data.Groups
-
-	tdata := tmplData{}
-
-	tdata.Title = "Groups"
-
-	fh, err := os.Create("groups.html")
+	fh, err := os.Create("report.html")
 	if err != nil {
 		return err
 	}
 	defer fh.Close()
 
-	var groupkeys []string
-	for key, group := range groups {
-		sort.Strings(group.Users)
-		groupkeys = append(groupkeys, key)
+	id := 0
+
+	root := &tmplData{
+		ChildrenMap: map[string]*tmplData{},
 	}
-	sort.Strings(groupkeys)
 
-	var userkeys []string
-	for key, user := range users {
-		sort.Strings(user.Groups)
-		userkeys = append(userkeys, key)
-	}
-	sort.Strings(userkeys)
-
-	tmplGroupIndex := map[string]*tmplDataGroup{}
-
-	for _, key := range groupkeys {
-		group := groups[key]
-		tgroup := &tmplDataGroup{
-			DN:   group.DN,
-			Name: group.Name,
+	for _, group := range data.Groups {
+		dn, err := ldap.ParseDN(group.DN)
+		if err != nil {
+			return err
 		}
-		tmplGroupIndex[key] = tgroup
-		tdata.Groups = append(tdata.Groups, tgroup)
+
+		tree := root
+		treedn := ""
+
+		for i := len(dn.RDNs) - 1; i >= 0; i-- {
+			rdn := dn.RDNs[i]
+			attr := rdn.Attributes[0]
+			subdn := attr.Type + "=" + ldap.EscapeFilter(attr.Value)
+			key := strings.ToLower(subdn)
+
+			if treedn == "" {
+				treedn = subdn
+			} else {
+				treedn = subdn + "," + treedn
+			}
+
+			if tree.ChildrenMap[key] == nil {
+				node := &tmplData{
+					ID:          fmt.Sprintf("node%d", id),
+					DN:          treedn,
+					SubDN:       subdn,
+					Name:        attr.Value,
+					ChildrenMap: map[string]*tmplData{},
+				}
+				id++
+				tree.ChildrenMap[key] = node
+			}
+
+			tree = tree.ChildrenMap[key]
+		}
+
+		for _, userkey := range group.Users {
+			user := data.Users[userkey]
+			key := strings.ToLower(user.Name)
+
+			if tree.ChildrenMap[key] == nil {
+				node := &tmplData{
+					ID:          fmt.Sprintf("node%d", id),
+					DN:          user.DN,
+					SubDN:       user.DN,
+					Name:        user.Name,
+					ChildrenMap: map[string]*tmplData{},
+				}
+				id++
+				tree.ChildrenMap[key] = node
+			}
+		}
 	}
 
-	for _, key := range userkeys {
-		user := users[key]
-		tuser := &tmplDataUser{
-			DN:   user.DN,
-			Name: user.Name,
+	/*	var groupkeys []string
+		for key, group := range groups {
+			sort.Strings(group.Users)
+			groupkeys = append(groupkeys, key)
 		}
-		for _, gk := range user.Groups {
-			tg := tmplGroupIndex[gk]
-			tg.Users = append(tg.Users, tuser)
-			tuser.Groups = append(tuser.Groups, tg)
-		}
-		tdata.Users = append(tdata.Users, tuser)
-	}
+		sort.Strings(groupkeys)
 
-	if err := tmpl.Execute(fh, tdata); err != nil {
+		var userkeys []string
+		for key, user := range users {
+			sort.Strings(user.Groups)
+			userkeys = append(userkeys, key)
+		}
+		sort.Strings(userkeys)
+
+		tmplGroupIndex := map[string]*tmplDataGroup{}
+
+		for _, key := range groupkeys {
+			group := groups[key]
+			tgroup := &tmplDataGroup{
+				DN:   group.DN,
+				Name: group.Name,
+			}
+			tmplGroupIndex[key] = tgroup
+			tdata.Groups = append(tdata.Groups, tgroup)
+		}
+
+		for _, key := range userkeys {
+			user := users[key]
+			tuser := &tmplDataUser{
+				DN:   user.DN,
+				Name: user.Name,
+			}
+			for _, gk := range user.Groups {
+				tg := tmplGroupIndex[gk]
+				tg.Users = append(tg.Users, tuser)
+				tuser.Groups = append(tuser.Groups, tg)
+			}
+			tdata.Users = append(tdata.Users, tuser)
+		}
+	*/
+
+	sortdata(root)
+
+	if err := tmpl.Execute(fh, root); err != nil {
 		return err
 	}
 
 	if err := fh.Close(); err != nil {
 		return err
 	}
+	return nil
+	/*
+		tdata.Title = "Users"
 
-	tdata.Title = "Users"
+		fh2, err := os.Create("users.html")
+		if err != nil {
+			return err
+		}
+		defer fh2.Close()
 
-	fh2, err := os.Create("users.html")
-	if err != nil {
-		return err
-	}
-	defer fh2.Close()
+		if err := tmpl2.Execute(fh2, tdata); err != nil {
+			return err
+		}
 
-	if err := tmpl2.Execute(fh2, tdata); err != nil {
-		return err
-	}
-
-	return fh2.Close()
+		return fh2.Close()*/
 }
